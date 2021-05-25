443296ba9e4a0c585a9596dbbcc03104dec4e0a4
Justin-F5972 <justinfernandez27@outlook.com>
2021-05-25 02:20:30 -0700

    made a semi-functional toDoList

--- js/main.js
+++ js/main.js
@@ -1,2 +1,44 @@
-var picker = datepicker("#date");
+window.onload = function () {
+    var buttonClick = document.getElementById("button");
+    buttonClick.onclick = addList;
+};
+var ToDoList = (function () {
+    function ToDoList() {
+    }
+    return ToDoList;
+}());
+var picker = datepicker("#due-date");
 picker.setMin(new Date());
+function createList() {
+    var list = new ToDoList();
+    var taskInput = document.getElementById("task");
+    list.task = taskInput.value;
+    var dueDateInput = document.getElementById("due-date");
+    list.dueDate = new Date(dueDateInput.value);
+    return list;
+}
+function displayList(myList) {
+    var displayDiv = document.getElementById("display");
+    var displayTask = document.createElement("h2");
+    displayTask.innerText = myList.task;
+    var displayDate = document.createElement("p");
+    displayDate.innerText = myList.dueDate.toDateString();
+    displayDiv.appendChild(displayTask);
+    displayDiv.appendChild(displayDate);
+}
+function addList() {
+    if (allDataValid()) {
+        var toDo = createList();
+        displayList(toDo);
+    }
+}
+function allDataValid() {
+    var taskInput = document.getElementById("task");
+    var task = taskInput.value;
+    if (task == "") {
+        var errorSpan1 = document.getElementById("task-span");
+        errorSpan1.innerText = "Please input a task name";
+        return false;
+    }
+    return true;
+}
--- ts/main.ts
+++ ts/main.ts
@@ -1,2 +1,62 @@
-const picker = datepicker("#date");
-            picker.setMin(new Date());
+window.onload = function(){
+    let buttonClick = <HTMLButtonElement>document.getElementById("button");
+    buttonClick.onclick = addList;
+}
+
+class ToDoList{
+    task:string;
+    dueDate:Date;
+}
+
+const picker = datepicker("#due-date");
+picker.setMin(new Date());
+
+// want to create a list item you can click after to show completion 
+function createList():ToDoList{
+    let list = new ToDoList();
+
+    let taskInput = <HTMLInputElement>document.getElementById("task");
+    list.task = taskInput.value;
+
+    let dueDateInput = <HTMLInputElement>document.getElementById("due-date");
+    list.dueDate = new Date(dueDateInput.value);
+
+    return list;
+}
+
+function displayList(myList:ToDoList):void{
+    let displayDiv = document.getElementById("display");
+
+    // Creates the task 
+    let displayTask = document.createElement("h2");
+    displayTask.innerText = myList.task;
+
+    // Shows the due Date
+    let displayDate = document.createElement("p");
+    displayDate.innerText = myList.dueDate.toDateString();
+
+    displayDiv.appendChild(displayTask);
+    displayDiv.appendChild(displayDate);
+}
+
+function addList(){
+    if(allDataValid()){
+        let toDo = createList();
+        displayList(toDo);
+    }
+}
+
+function allDataValid():boolean{
+    let taskInput = <HTMLInputElement>document.getElementById("task");
+    let task = taskInput.value;
+
+    if(task == ""){
+        let errorSpan1 = <HTMLSpanElement>document.getElementById("task-span");
+        errorSpan1.innerText = "Please input a task name"
+        return false;
+    }
+
+    return true;
+}
+
+//make a delete for completed tasks
